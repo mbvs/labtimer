ce33f2a024bf2af7c3fd0cbb00955663b51aa89f
Marcus Bauer von Schildhaue <marcus@kalliope.local>
2019-12-31 21:56:10 +0100

    minor tweak

--- src/main.c
+++ src/main.c
@@ -1,30 +1,28 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define LED      PB0
-#define LED_DDR  DDRB
+#define LED PB0
+#define LED_DDR DDRB
 #define LED_PORT PORTB
 
-#define DELAYTIME 200
+#define DELAYTIME 100
 
-#define setBit(sfr, bit)     (_SFR_BYTE(sfr) |= (1 << bit))
-#define clearBit(sfr, bit)   (_SFR_BYTE(sfr) &= ~(1 << bit))
-#define toggleBit(sfr, bit)  (_SFR_BYTE(sfr) ^= (1 << bit))
+#define setBit(sfr, bit) (_SFR_BYTE(sfr) |= (1 << bit))
+#define clearBit(sfr, bit) (_SFR_BYTE(sfr) &= ~(1 << bit))
+#define toggleBit(sfr, bit) (_SFR_BYTE(sfr) ^= (1 << bit))
 
-int main(void) {
+int main(void)
+{
 
   // Init
-  setBit(LED_DDR, LED);                      /* set LED pin for output */
+  setBit(LED_DDR, LED); /* set LED pin for output */
 
   // Mainloop
-  while (1) {
+  while (1)
+  {
 
-    setBit(LED_PORT, LED);
     _delay_ms(DELAYTIME);
-
-    clearBit(LED_PORT, LED);
-    _delay_ms(DELAYTIME);
-
+    toggleBit(LED_PORT, LED);
   }
-  return 0;                                  /* end mainloop */
+  return 0; /* end mainloop */
 }
